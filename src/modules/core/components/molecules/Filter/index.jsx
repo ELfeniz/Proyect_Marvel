b4bd9bf91eedba45a15712a72fb066961d03d388
ELfeniz <juanpavilla1999@gmail.com>
2022-12-05 01:22:05 -0500

    modificacion barra de busqueda

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -1,7 +1,27 @@
+import './styles.scss';
+import { BsSearch } from 'react-icons/bs';
+
 export default function Filter() {
   return (
     <div className="mvl-character-gri-filters">
-      <input type="text" placeholder="search" />
+      <div className="contenerdor_search">
+        <div className="linea">
+          <BsSearch className="icon" />
+          <input type="text" className="color_f" placeholder="SEARCH" />
+        </div> 
+      </div>
+      <div className="contenerdor_2">
+        <BsSearch className="icon2" />
+        <BsSearch className="icon2" />
+      </div>
+      <div className="contenerdor_3">
+        <BsSearch className="icon2" />
+        <BsSearch className="icon2" />
+      </div>
+      <div className="contenerdor_4">
+        <BsSearch className="icon2" />
+        <BsSearch className="icon2" />
+      </div>
     </div>
   );
 }
